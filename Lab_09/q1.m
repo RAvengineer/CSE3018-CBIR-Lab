241eaa56902cc9e7a9b3579140dc025a04b20744
Rahul Bera <bera2591@gmail.com>
2021-04-29 16:06:25 +0530

    9-q1.m: Add barplot to support visualization of the features

--- Lab_09/q1.m
+++ Lab_09/q1.m
@@ -1,8 +1,13 @@
 % Extract LBP features from a image
 
 clc;
+
 imgFilePath = "../image_set_2/1.2.01.tiff";
 img = imread(imgFilePath);
 feat = lbp(img, 1);
-disp(size(img));
-disp(sum(feat));
+
+figure('Name','LBP features','NumberTitle','off');
+bar(feat);
+title(sprintf('LBP features for %s', imgFilePath))
+xlabel('Feature Bins');
+ylabel('Frequency');

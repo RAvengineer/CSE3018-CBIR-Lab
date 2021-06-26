77cdf7a4e992753c9aa50066a8eb94d2930817a1
Rahul Bera <bera2591@gmail.com>
2021-06-26 17:42:36 +0530

    Lab_12/SURF.m: Replace static file path for input images with dynamic GUI filepicker to extract the file paths

--- Lab_12/SURF.m
+++ Lab_12/SURF.m
@@ -1,20 +1,36 @@
 clf
-img=imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_1/img13.jpg');
-scene_img=im2gray(img);
+% Select Scene image
+[scene_name, file_path] = uigetfile('*.*', 'Select the scene image');
+scene_img_path = sprintf("%s%s", file_path, scene_name);
+if scene_name
+    scene_img = im2gray(imread(scene_img_path));
+else
+    disp('No scene image selected!');
+    return
+end
+% Extract SURF features for scene image
 points = detectSURFFeatures(scene_img);
 subplot(1, 2, 1);
 imshow(scene_img); hold on;
 title('Scene Image');
-%Extracting top 100 points
+% Extracting top 100 points
 plot(points.selectStrongest(100));
 
-img2=imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_1/img1.jpg');
-target_img=im2gray(img2);
+% Select Target image
+[target_name, file_path] = uigetfile('*.*', 'Select the scene image');
+target_path = sprintf("%s%s", file_path, target_name);
+if target_name
+    target_img = im2gray(imread(target_path));
+else
+    disp('No scene image selected!');
+    return
+end
+% Extract SURF features for target image
 points2 = detectSURFFeatures(target_img);
 subplot(1, 2, 2);
 imshow(target_img); hold on;
 title('Target Image');
-%Extracting top 300 points
+% Extracting top 300 points
 plot(points2.selectStrongest(300));
 
 

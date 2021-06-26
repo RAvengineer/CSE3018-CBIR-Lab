5214cb6f40102dcaa171dcdb46c584dbd466cdc2
Rahul Bera <bera2591@gmail.com>
2021-06-26 18:21:50 +0530

    Lab_12/SURF.m: Add display method for matched feat - Add code for using rotated scene image as target image - Provide side-by-side comparision for matched features - Add title for displayed matched features

--- Lab_12/SURF.m
+++ Lab_12/SURF.m
@@ -1,6 +1,6 @@
 clf
-%% Extract SURF Features
-% Select Scene image
+% Extract SURF Features
+%% Select Scene image
 [scene_name, file_path] = uigetfile('*.*', 'Select the scene image');
 scene_img_path = sprintf("%s%s", file_path, scene_name);
 if scene_name
@@ -17,8 +17,12 @@ title('Scene Image');
 % Extracting top 100 points
 plot(points.selectStrongest(100));
 
-% Select Target image
-[target_name, file_path] = uigetfile('*.*', 'Select the scene image');
+%% Select Target image
+% Either pick a new image or rotate the scene image
+%% For rotating the scene image, uncomment the line below
+% target_img = imresize(imrotate(scene_img, -20), 1.2);
+%% For picking the a new target image
+[target_name, file_path] = uigetfile('*.*', 'Select the target image');
 target_path = sprintf("%s%s", file_path, target_name);
 if target_name
     target_img = im2gray(imread(target_path));
@@ -35,22 +39,17 @@ title('Target Image');
 plot(points2.selectStrongest(300));
 
 %% Matching images
-
-% I2 = imresize(imrotate(I1,-20),1.2); %rotation of the input image.
-
-%Extracting the features.
+% Extracting the features
 [f1,vpts1] = extractFeatures(scene_img, points1);
 [f2,vpts2] = extractFeatures(target_img, points2);
 
-%Retrieving the locations of matched points.
+% Retrieving the locations of matched points
 indexPairs = matchFeatures(f1,f2) ;
 matchedPoints1 = vpts1(indexPairs(:,1));
 matchedPoints2 = vpts2(indexPairs(:,2));
 
-%Displaying the matching points.
-figure;
-showMatchedFeatures(scene_img, target_img, matchedPoints1, matchedPoints2);
-legend('matched points 1','matched points 2');
-
-
-
+% Displaying the matching points
+figure; ax = axes;
+showMatchedFeatures(scene_img, target_img, matchedPoints1, matchedPoints2, 'montage','Parent',ax);
+title(ax, 'Candidate point matches');
+legend(ax, 'Matched points 1','Matched points 2');

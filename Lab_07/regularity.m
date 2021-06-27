d755b64512d973f309d4b9ee38e49e5347c551b3
Rahul Bera <42903856+RAvengineer@users.noreply.github.com>
2021-06-27 23:29:30 +0530

    Lab_07: Add Tamura feature calculation functions Code copied from https://github.com/MarshalLeeeeee/Tamura-In-Python/tree/master/referenced-matlab-code

---
+++ Lab_07/regularity.m
@@ -0,0 +1,23 @@
+%调用举例：  
+%image=rgb2gray(imread('example.jpg'));  
+%Freg=regularity(image,64)  
+function Freg=regularity(graypic,windowsize) %windowsize为计算规则度的子窗口大小  
+    [h,w]=size(graypic);  
+    k=0;  
+    for i=1:windowsize:h-windowsize  
+        for j=1:windowsize:w-windowsize  
+            k=k+1;  
+            crs(k)=coarseness(graypic(i:i+windowsize-1,j:j+windowsize-1),5); %粗糙度  
+            con(k)=contrast(graypic(i:i+windowsize-1,j:j+windowsize-1)); %对比度  
+            [dire(k),sita]=directionality(graypic(i:i+windowsize-1,j:j+windowsize-1));%方向度  
+            lin=linelikeness(graypic(i:i+windowsize-1,j:j+windowsize-1),sita,4)*10; %线性度，*10与crs、con、dire同量级化  
+        end
+    end  
+    % Find the standard deviation of the above parameters 
+    Dcrs=std(crs,1);  
+    Dcon=std(con,1);  
+    Ddir=std(dire,1);  
+    Dlin=std(lin,1);
+
+    Freg=1-(Dcrs+Dcon+Ddir+Dlin)/4/100;
+end

5aec34f8ee327863b200bc89d69c860611b431c4
Rahul Bera <42903856+RAvengineer@users.noreply.github.com>
2021-06-29 02:14:00 +0530

    Lab_12/SIFT.m: Add file picker & fix resize & axes bug - Replace hard-coded filepaths with uigetfile file picker - Add sections for resp. code - Fix errors caused when resizing color images - Fix bug: Image matching line inconsistently being display on any figure. Fix the bug by isolating the axes for the plot - Rename variables - Reformat the code

--- Lab_12/SIFT.m
+++ Lab_12/SIFT.m
@@ -1,48 +1,76 @@
 clc;
 % Extract SIFT Features
 
-I = imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_2/1.1.12.tiff');
-figure('Name', 'SIFT frames & descriptors', 'NumberTitle', 'off'); 
-imshow(I);
-I = single(im2gray(I));
-% compute the SIFT frames (keypoints) and descriptors
-[f,d] = vl_sift(I);
-perm = randperm(size(f,2));
+%% Select Scene image
+[scene_name, file_path] = uigetfile('*.*', 'Select the scene image');
+scene_img_path = sprintf("%s%s", file_path, scene_name);
+if scene_name
+    scene_img = imread(scene_img_path);
+    scene_img_gray = single(im2gray(scene_img));
+else
+    disp('No scene image selected!');
+    return
+end
+
+%% Single image SIFT feature extraction
+figure('Name', 'SIFT frames & descriptors', 'NumberTitle', 'off');
+imshow(scene_img); % Display image
+
+% Compute the SIFT frames (keypoints) and descriptors
+[f,d] = vl_sift(scene_img_gray);
+perm = randperm(size(f, 2));
 sel = perm(1:50);
-h1 = vl_plotframe(f(:,sel));
-h2 = vl_plotframe(f(:,sel));
-set(h1,'color','k','linewidth',3);
-set(h2,'color','y','linewidth',2);
-h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel));
+h1 = vl_plotframe(f(:, sel));
+h2 = vl_plotframe(f(:, sel));
+set(h1, 'color', 'k', 'linewidth', 3);
+set(h2, 'color', 'y', 'linewidth', 2);
+h3 = vl_plotsiftdescriptor(d(:,sel), f(:,sel));
 set(h3,'color','g');
 
-%% Image Matching
+%% Select Target image
+% Either pick a new image or rotate the scene image
+% For rotating the scene image, uncomment the line below
+% target_img = imresize(imrotate(scene_img, -20), 1.2);
+% For picking the a new target image
+[target_name, file_path] = uigetfile('*.*', 'Select the target image');
+target_path = sprintf("%s%s", file_path, target_name);
+if target_name
+    target_img = imread(target_path);
+else
+    disp('No target image selected!');
+    return
+end
+
 figure('Name', 'Selected Images', 'NumberTitle', 'off');
-Ia = imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_2/1.1.12.tiff');
-subplot(1,2,1); imshow(Ia);
-[r, c] = size(Ia);
-Ib = imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_2/1.3.09.tiff');
-Ib = imresize(Ib, [r c]);
-subplot(1,2,2); imshow(Ib);
-Ia = single(im2gray(Ia));
-Ib = single(im2gray(Ib));
-%NOTE: Images have to be the same size
-[fa, da] = vl_sift(Ia);
-[fb, db] = vl_sift(Ib);
-[matches, scores] = vl_ubcmatch(da, db);
+subplot(1,2,1); imshow(scene_img);  % Display scene image
+
+
+%% Image Matching
+
+% --- Resize target image ---
+% scene_img_size = size(scene_img);
+% target_img_size = scene_img_size(1:2);    % => size(scene_img_gray)
+target_img = imresize(target_img, size(scene_img_gray));
+subplot(1,2,2); imshow(target_img); % Display target image
+target_img_gray = single(im2gray(target_img));
+
+% --- NOTE: Images have to be the same size ---
+[fs, ds] = vl_sift(scene_img_gray);
+[ft, dt] = vl_sift(target_img_gray);
+[matches, scores] = vl_ubcmatch(ds, dt);
 
 %% Visualization
 figure('Name', 'Image Matching Visulization', 'NumberTitle', 'off');
 ax = axes;
-m1 = fa (1:2,matches(1,:));
-m2 = fb(1:2,matches(2,:));
-m2(1,:) = m2(1,:) + size(Ia,2) * ones(1,size(m2,2));
+m1 = fs(1:2,matches(1,:));
+m2 = ft(1:2,matches(2,:));
+m2(1,:) = m2(1,:) + size(scene_img_gray,2) * ones(1,size(m2,2));
 X = [m1(1,:);m2(1,:)];
 Y = [m1(2,:);m2(2,:)];
-c = [Ia Ib];
+c = [scene_img target_img];
 imshow(c,[]); 
 hold on;
 
 % Determine number of lines depending on dimensions of X and Y
-line(ax, X,Y);
+line(ax, X, Y);
 

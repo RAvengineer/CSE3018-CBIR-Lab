762759a95874016f4d7e20e4a4b78fe621bc0701
Rahul Bera <42903856+RAvengineer@users.noreply.github.com>
2021-06-29 01:20:27 +0530

    Lab_12/SIFT.m: Modify output visualization

--- Lab_12/SIFT.m
+++ Lab_12/SIFT.m
@@ -2,7 +2,8 @@ clc;
 % Extract SIFT Features
 
 I = imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_2/1.1.12.tiff');
-image(I);
+figure('Name', 'SIFT frames & descriptors', 'NumberTitle', 'off'); 
+imshow(I);
 I = single(im2gray(I));
 % compute the SIFT frames (keypoints) and descriptors
 [f,d] = vl_sift(I);
@@ -16,12 +17,13 @@ h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel));
 set(h3,'color','g');
 
 %% Image Matching
+figure('Name', 'Selected Images', 'NumberTitle', 'off');
 Ia = imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_2/1.1.12.tiff');
-figure; image(Ia);
+subplot(1,2,1); imshow(Ia);
 [r, c] = size(Ia);
-Ib = imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_2/1.3.12.tiff');
+Ib = imread('/home/rahul/VITCC/WIN_20-21/CSE3018/Lab/image_set_2/1.3.09.tiff');
 Ib = imresize(Ib, [r c]);
-figure; image(Ib);
+subplot(1,2,2); imshow(Ib);
 Ia = single(im2gray(Ia));
 Ib = single(im2gray(Ib));
 %NOTE: Images have to be the same size
@@ -30,15 +32,17 @@ Ib = single(im2gray(Ib));
 [matches, scores] = vl_ubcmatch(da, db);
 
 %% Visualization
+figure('Name', 'Image Matching Visulization', 'NumberTitle', 'off');
+ax = axes;
 m1 = fa (1:2,matches(1,:));
 m2 = fb(1:2,matches(2,:));
 m2(1,:) = m2(1,:) + size(Ia,2) * ones(1,size(m2,2));
 X = [m1(1,:);m2(1,:)];
 Y = [m1(2,:);m2(2,:)];
 c = [Ia Ib];
-imshow(c,[]);
+imshow(c,[]); 
 hold on;
 
 % Determine number of lines depending on dimensions of X and Y
-line(X,Y);
+line(ax, X,Y);
 
